97d0b3bf8fab9928802c18de7c5509ba74c6d476
Vassily <vassily.v.v@gmail.com>
2021-10-11 19:13:08 +0600

    init commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# 1. Create cluster
+# 2. Destine kubectl to the cluster
+# 3. Run following commands:
+export RELAY_KEY=***-***-***-****-****
+export RELAY_SECRET=****
+
+helm repo add webhookrelay https://charts.webhookrelay.com
+# docker login must be already executed 
+kubectl create secret generic regcred \
+    --from-file=.dockerconfigjson=/home/zuber/.docker/config.json \
+    --type=kubernetes.io/dockerconfigjson
+
+kubectl create -f secrets/dev/backend.yaml
+
+helm upgrade --install webhookrelay-operator webhookrelay/webhookrelay-operator \
+  --set credentials.key=$RELAY_KEY --set credentials.secret=$RELAY_SECRET
+  
+helm upgrade --install keel keel/keel --set helmProvider.enabled="false" --set service.enabled="true" --set service.type="ClusterIP"
+
+kubectl create -f cd/prod/cd.yaml
+kubectl create -f deployments/prod/app.yaml
+
+kubectl exec --stdin --tty sunpay-frontend-dev-deployment-7fbb5ff955-npqhm -- /bin/bash
---
+++ k8s/secrets/make_secrets.py
@@ -0,0 +1,23 @@
+import os
+from base64 import b64encode
+
+header = """apiVersion: v1
+kind: Secret
+metadata:
+  name: {}-secret
+type: kubernetes.io/Opaque
+data:
+"""
+
+env_file_names = filter(lambda x: '.env' in x and 'sample' not in x, filter(os.path.isfile, os.listdir(os.curdir)))
+for env_file_name in env_file_names:
+    name = env_file_name.split('.')[0]
+    data = header.format(name)
+    with open(env_file_name, 'r') as env_file:
+        lines = env_file.read().split('\n')
+        for line in lines:
+            if line:
+                key, value = line.split('=')
+                data += f'    {key}: {b64encode(value.encode("utf-8")).decode("utf-8")}\n'
+    with open(f'{name}.yaml', 'w') as secret_file:
+        secret_file.write(data)
