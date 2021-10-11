70497c4e3c010288b017e9eb701d4558590ea0b0
Vassily <vassily.v.v@gmail.com>
2021-10-12 00:32:23 +0600

    jwt RS256

--- k8s/secrets/make_secrets.py
+++ k8s/secrets/make_secrets.py
@@ -1,3 +1,4 @@
+import base64
 import os
 from base64 import b64encode
 
@@ -19,5 +20,8 @@ for env_file_name in env_file_names:
             if line:
                 key, value = line.split('=')
                 data += f'    {key}: {b64encode(value.encode("utf-8")).decode("utf-8")}\n'
+
+    data += f'    BAD_RS_PRIVATE_KEY: {bad_private_key_double_base64_encoded}\n'
+    data += f'    GOOD_RS_PRIVATE_KEY: {good_private_key_double_base64_encoded}\n'
     with open(f'{name}.yaml', 'w') as secret_file:
         secret_file.write(data)
